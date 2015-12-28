395bdbeccc961d00a15657a3e992d759c0007b54
Adam Terrey <arterrey@gmail.com>
2015-12-28 16:31:39 +0700

    made the pyramid application work

--- src/gaebuildoutdemo/app/main.py
+++ src/gaebuildoutdemo/app/main.py
@@ -1,11 +1,11 @@
 
-from gaebuildoutdemo.app.view import welcome_view
+
 
 from pyramid.config import Configurator
 
 
 def app_factory(global_config, **settings):
     config = Configurator(settings=settings)
-    config.add_route('hello', '/hello')
-    config.add_view(welcome_view, 'hello')
+    config.add_route('hello', '/')
+    config.scan()
     return config.make_wsgi_app()
--- src/gaebuildoutdemo/app/view.py
+++ src/gaebuildoutdemo/app/view.py
@@ -1,8 +1,11 @@
 
 
+from pyramid.view import view_config
 
 from pyramid.response import Response
 
 
+@view_config(route_name='hello')
 def welcome_view(request):
     return Response('Hi :) This is a test pyramid application!')
+

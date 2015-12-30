02fd716bd1835288eda00d3181bbbece120f425d
Adam Terrey <arterrey@gmail.com>
2015-12-30 14:25:08 +0700

    sorted datastore entries

--- src/adamandpaul/gwebdemo/view.py
+++ src/adamandpaul/gwebdemo/view.py
@@ -10,6 +10,6 @@ from num2words import num2words
 
 def welcome_view(request):
     population_info = []
-    for pop in Population.all().fetch(100):
+    for pop in Population.all().order('-count').fetch(100):
         population_info.append({'country': pop.country, 'count': num2words(pop.count).title()})
     return {'project': 'adamandpaul.gwebdemo', 'populations': population_info}
